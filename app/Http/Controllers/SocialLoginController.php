355619f4402306438adb27b430bbcf3f8d9d4f8b
Mamunur1093 <mamunur1093@gmail.com>
2023-12-22 11:41:10 +0600

    controller update

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
-class SocialiteLoginController extends Controller
+class SocialLoginController extends Controller
 {
     public function githubredirect()
     {
@@ -15,19 +17,24 @@ class SocialiteLoginController extends Controller
 
     public function githubcallback()
     {
-        $user = Socialite::driver('github')->user();
-        // dd($user);
-        $user = User::updateOrCreate([
-            'github_id' => $user->id,
-        ], [
-            'name' => $user->name,
-            'email' => $user->email,
-            'password' => encrypt('Test@123'),
-        ]);
+        try {
+            $user = Socialite::driver('github')->user();
+            // dd($user);
+            $user = User::updateOrCreate([
+                'github_id' => $user->id,
+            ], [
+                'name' => $user->name,
+                'email' => $user->email,
+                'password' => Hash::make($user->id),
+            ]);
 
-        Auth::login($user);
+            Auth::login($user);
 
-        return redirect('/dashboard');
+            return redirect(RouteServiceProvider::HOME);
+        } catch (user) {
+            return redirect()->route('login')->with('message', 'Credencial not match');
+        }
+
     }
 
     public function googleredirect()
@@ -38,18 +45,19 @@ class SocialiteLoginController extends Controller
     public function googlecallback()
     {
         $user = Socialite::driver('google')->user();
-
+        // dd($user);
         $user = User::updateOrCreate([
             'google_id' => $user->id,
         ], [
             'name' => $user->name,
             'email' => $user->email,
-            'password' => encrypt('Test@123'),
+            'password' => Hash::make($user->id),
+            'image' => $user->picture,
         ]);
 
         Auth::login($user);
 
-        return redirect('/dashboard');
+        return redirect(RouteServiceProvider::HOME);
     }
 
     public function faceboockredirect()
@@ -66,11 +74,11 @@ class SocialiteLoginController extends Controller
         ], [
             'name' => $user->name,
             'email' => $user->email,
-            'password' => encrypt('Test@123'),
+            'password' => Hash::make($user->id),
         ]);
 
         Auth::login($user);
 
-        return redirect('/dashboard');
+        return redirect(RouteServiceProvider::HOME);
     }
 }
