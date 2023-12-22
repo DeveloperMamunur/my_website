bb01a5164e4a20e3affd40ef79112b969c41b5ca
Mamunur1093 <mamunur1093@gmail.com>
2023-12-22 11:40:47 +0600

    vite css update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/index.css',
                 'resources/css/app.css',
-                'resources/css/login.js',
+                'resources/css/login.css',
                 'resources/js/app.js',
             ],
             refresh: true,
